f434f312e75d10870794f6d51dcbb7140d510d93
Lestt <jonathan.clus@holbertonschool.com>
2022-11-25 16:46:43 +1100

    task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "hash_tables.h"
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *head;
+	unsigned long int index;
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+
+	while(head != NULL)
+	{
+		if (strcmp(head->key, key) == 0)
+		{
+			return (head->value);
+		}
+		head = head->next;
+	}
+	return (NULL);
+}
